2e42154aaf3a81f20ca64680a75775bf791b8f47
Christopher Griesch <christopher-griesch@fc-hansa.de>
2024-05-07 16:02:30 +0200

    * bugfix

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,25 +1,17 @@
 <?php
-declare(strict_types=1);
 
-/*
- * This file is part of Contao.
- *
- * (c) Leo Feyer
- *
- * @license LGPL-3.0-or-later
- */
+declare(strict_types=1);
 
 namespace Codebuster\ContaoOpenaiBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
-use Codebuster\ContaoOpenaiBundle\ContaoOpenaiBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Routing\RouteCollection;
+use Codebuster\ContaoOpenaiBundle\ContaoOpenaiBundle;
 
 /**
  * Plugin for the Contao Manager.
@@ -35,14 +27,15 @@ class Plugin implements BundlePluginInterface, RoutingPluginInterface
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(ContaoOpenaiBundle::class)->setLoadAfter([ContaoCoreBundle::class]),
+            (new BundleConfig(ContaoOpenaiBundle::class))
+                ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel): ?RouteCollection
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
     {
         return $resolver
             ->resolve(__DIR__.'/../Resources/config/routes.yml')
